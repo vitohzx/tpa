edf279b2056663ccac7f802202ef5bec9b730e28
Vitor <163857646+vitohzx@users.noreply.github.com>
2024-09-16 18:16:00 -0300

    Delete 3_bimestre directory

--- 3_bimestre/aula1_revisao/exercicio3.js
+++
@@ -1,5 +0,0 @@
-let p2 = 1
-let p1 = 2 
-let soma = p1 + p2
-
-console.log (soma)
--- 3_bimestre/aula2_condicionais/exemplo3.js
+++
@@ -1,7 +0,0 @@
-let media = 4.0;
-
-if (media >= 5) {
-    console.log ("Voce foi Aprovado(a)")
-} else {
-    console.log ("Voce foi reprovado(a)")
-}
